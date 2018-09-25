0fe46164569ecf84df1b22d3289257a5e14e6c6c
fluffytoycoy <40921499+fluffytoycoy@users.noreply.github.com>
2018-09-25 00:27:48 -0500

    Add files via upload
    Fixed a few lines added event bubble

---
+++ FormUI/Dashboard.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+    public partial class Dashboard : Form
+    {
+        List<Movie> Movies = new List<Movie>();
+        bool MovieTabOpening = true;
+        bool ActorTabOpening = true;
+        bool DirectorTabOpening = true;
+        
+    
+
+    public void MyEventHandlerFunction_StatusUpdated(object sender, EventArgs e)
+    {
+            DataAccessSelect db = new DataAccessSelect();
+            Movies = db.GetMovies();
+            dataGridView2.DataSource = Movies;
+        }
+
+    public Dashboard()
+        {
+            
+            InitializeComponent();
+            this.searchByDirectorForm1.StatusUpdated += new EventHandler(MyEventHandlerFunction_StatusUpdated);
+
+
+        }
+
+        //private void ShowAllMoviesBtn_Click(object sender, EventArgs e)
+        //{
+        //    DataAccessSelect db = new DataAccessSelect();
+        //    Movies = db.GetMovies();
+
+        //}
+
+        //private void SearchByDirectorBtn_Click(object sender, EventArgs e)
+        //{
+        //    DataAccessSelect db = new DataAccessSelect();
+        //    try
+        //    {
+        //        Movies = db.GetMovieByDirector(DirFirstNameTextBox.Text, DirLastNameTextBox.Text);
+        //        UpdateList();
+        //    }
+        //    catch
+        //    {
+        //        MessageBox.Show("No Director Entry was made");
+        //    }
+            
+        //}
+
+        private void UpdateList()
+        {
+
+        }
+
+        //private void SearchByActorBtn_Click(object sender, EventArgs e)
+        //{
+        //    DataAccessSelect db = new DataAccessSelect();
+        //    try
+        //    {
+        //        Movies = db.GetMovieByActor(ActorFirstNameTextBox.Text, ActorLastNameTextBox.Text);
+        //        UpdateList();
+                
+        //    }
+        //    catch
+        //    {
+        //        MessageBox.Show("No Actor Entry was made");
+        //    }
+        //}
+
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            DropDownTimer.Start();
+        }
+
+        private void DropDownTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (!MovieTabOpening)
+            {
+                MoviePanel.Height -= 11;
+                if (MoviePanel.Height <= 0)
+                {
+                    MoviePanel.Height = 0;
+                    MovieTabOpening = true;
+                    DropDownTimer.Stop();
+                }
+            }
+            else if (MovieTabOpening)
+            {
+                MoviePanel.Height += 11;
+                if (MoviePanel.Height >= 132)
+                {
+                    MoviePanel.Height = 132;
+                    MovieTabOpening = false;
+                    DropDownTimer.Stop();
+                }
+            }
+
+
+        }
+
+        private void ActorDropDownBtn_Click(object sender, EventArgs e)
+        {
+            ActorDropDownTimer.Start();
+        }
+
+        private void ActorDropDownTimer_Tick(object sender, EventArgs e)
+        {
+            if (!ActorTabOpening)
+            {
+                ActorPanel.Height -= 11;
+                if (ActorPanel.Height <= 0)
+                {
+                    ActorPanel.Height = 0;
+                    ActorTabOpening = true;
+                    ActorDropDownTimer.Stop();
+                }
+            }
+            else if (ActorTabOpening)
+            {
+                ActorPanel.Height += 11;
+                if (ActorPanel.Height >= 132)
+                {
+                    ActorPanel.Height = 132;
+                    ActorTabOpening = false;
+                    ActorDropDownTimer.Stop();
+                }
+            }
+        }
+
+        private void DirectorDropDownBtn_Click(object sender, EventArgs e)
+        {
+            DirectorDropDownTimer.Start();
+        }
+
+        private void DirectorDropDownTimer_Tick(object sender, EventArgs e)
+        {
+            if (!DirectorTabOpening)
+            {
+                DirectorPanel.Height -= 11;
+                if (DirectorPanel.Height <= 0)
+                {
+                    DirectorPanel.Height = 0;
+                    DirectorTabOpening = true;
+                    DirectorDropDownTimer.Stop();
+                }
+            }
+            else if (DirectorTabOpening)
+            {
+                DirectorPanel.Height += 11;
+                if (DirectorPanel.Height >= 132)
+                {
+                    DirectorPanel.Height = 132;
+                    DirectorTabOpening = false;
+                    DirectorDropDownTimer.Stop();
+                }
+            }
+        }
+
+        private void AllMoviesBtn_Click(object sender, EventArgs e)
+        {
+            DropDownTimer.Start();
+            DataAccessSelect db = new DataAccessSelect();
+            Movies = db.GetMovies();
+            dataGridView2.DataSource = Movies;
+
+        }
+    }
+   
+}
---
+++ FormUI/DataAccess/DataAccessSelect.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+
+namespace FormUI
+{
+    public class DataAccessSelect
+    {
+
+        public List<Movie> GetMovies()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnValue("MyMediaDB")))
+            {
+                var output = connection.Query<Movie>("dbo.spSelect_All_Movies").ToList();
+                return output;
+            }
+        }
+
+        public List<Movie> GetMovieByDirector(string firstName, string lastName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnValue("MyMediaDB")))
+            {
+                List<Movie> output = null;
+                if (firstName != "" && lastName != "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Director @FirstName, @LastName", 
+                        new { FirstName = firstName, LastName = lastName }).ToList();
+                }
+                else if (firstName == "" && lastName != "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Director_LastName  @LastName",
+                        new { LastName = lastName }).ToList();
+                }
+                else if (firstName != "" && lastName == "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Director_FirstName @FirstName",
+                      new { FirstName = firstName }).ToList(); 
+                }
+                else
+                {
+                    throw new Exception();
+                    
+                }
+
+                
+                return output;
+            }
+        }
+
+        public  List<Movie> GetMovieByActor(string firstName, string lastName)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnValue("MyMediaDB")))
+            {
+                List<Movie> output = null;
+                if (firstName != "" && lastName != "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Actor @FirstName, @LastName",
+                        new { FirstName = firstName, LastName = lastName }).ToList();
+                }
+                else if (firstName == "" && lastName != "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Actor_LastName  @LastName",
+                        new { LastName = lastName }).ToList();
+                }
+                else if (firstName != "" && lastName == "")
+                {
+                    output = connection.Query<Movie>("dbo.spSearch_By_Actor_FirstName @FirstName",
+                      new { FirstName = firstName }).ToList();
+                }
+                else
+                {
+                    throw new Exception();
+
+                }
+
+
+                return output;
+            }
+        }
+    }
+}
